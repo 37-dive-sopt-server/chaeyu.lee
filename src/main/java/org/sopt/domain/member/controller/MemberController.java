d11168b4a2d7e3e51731610b499745a6e31e3e5f
Chae-Yu <chaeyu2004@naver.com>
2025-10-30 13:18:45 +0900

    refactor: ResponseEntity 삭제

--- src/main/java/org/sopt/domain/member/controller/MemberController.java
+++ src/main/java/org/sopt/domain/member/controller/MemberController.java
@@ -4,8 +4,6 @@ import org.sopt.domain.member.dto.request.MemberCreateRequestDto;
 import org.sopt.domain.member.dto.response.MemberResponseDto;
 import org.sopt.global.response.BaseResponse;
 import org.sopt.domain.member.service.MemberService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,28 +18,27 @@ public class MemberController {
     }
 
     @PostMapping()
-    public ResponseEntity<BaseResponse<Long>> createMember(@RequestBody MemberCreateRequestDto memberCreateRequestDto) {
+    public BaseResponse<Long> createMember(@RequestBody MemberCreateRequestDto memberCreateRequestDto) {
         Long id = memberService.join(memberCreateRequestDto);
         memberService.close();
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(BaseResponse.create("회원 등록 성공", id));
+        return BaseResponse.create("회원 등록 성공", id);
     }
 
     @GetMapping("/{memberId}")
-    public ResponseEntity<BaseResponse<MemberResponseDto>> findMemberById(@PathVariable Long memberId) {
+    public BaseResponse<MemberResponseDto> findMemberById(@PathVariable Long memberId) {
         MemberResponseDto member = memberService.findOne(memberId);
-        return ResponseEntity.ok(BaseResponse.ok("회원 조회 성공", member));
+        return BaseResponse.ok("회원 조회 성공", member);
     }
 
     @GetMapping("/all")
-    public ResponseEntity<BaseResponse<List<MemberResponseDto>>> getAllMembers() {
+    public BaseResponse<List<MemberResponseDto>> getAllMembers() {
         List<MemberResponseDto> members = memberService.findAllMembers();
-        return ResponseEntity.ok(BaseResponse.ok("전체 회원 조회 성공", members));
+        return BaseResponse.ok("전체 회원 조회 성공", members);
     }
 
     @DeleteMapping("/{memberId}")
-    public ResponseEntity<BaseResponse<Boolean>> deleteMember(@PathVariable Long memberId){
+    public BaseResponse<Boolean> deleteMember(@PathVariable Long memberId){
         memberService.deleteMember(memberId);
-        return ResponseEntity.ok(BaseResponse.ok("회원 삭제 성공", true));
+        return BaseResponse.ok("회원 삭제 성공", true);
     }
 }
