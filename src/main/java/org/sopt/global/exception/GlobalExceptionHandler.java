6b7594199139e8dfa764d8e0eb509578ec6dee68
Chae-Yu <chaeyu2004@naver.com>
2025-12-18 15:47:03 +0900

    feat: 에러 로그 추가

--- src/main/java/org/sopt/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.sopt.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.sopt.global.exception.constant.ErrorCode;
 import org.sopt.global.exception.constant.GlobalErrorCode;
 import org.sopt.global.response.BaseErrorResponse;
@@ -9,6 +10,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -16,6 +18,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<BaseErrorResponse> handlerCustomException(CustomException ex){
         ErrorCode errorCode = ex.getErrorCode();
+        log.warn("CustomException: Code: {}, Message: {}", errorCode.getCode(), errorCode.getMsg());
         return ResponseEntity.status(errorCode.getHttpStatus())
                 .body(BaseErrorResponse.of(errorCode));
     }
@@ -23,6 +26,7 @@ public class GlobalExceptionHandler {
     // 모든 예외
     @ExceptionHandler(Exception.class)
     public ResponseEntity<BaseErrorResponse> handlerInternalServerError(Exception ex) {
+        log.error("Internal Server Error: ", ex);
         return ResponseEntity.status(GlobalErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
                 .body(BaseErrorResponse.of(GlobalErrorCode.INTERNAL_SERVER_ERROR));
     }
@@ -30,12 +34,14 @@ public class GlobalExceptionHandler {
     // 타입 불일치
     @ExceptionHandler(TypeMismatchException.class)
     public BaseErrorResponse handleTypeMismatch(TypeMismatchException ex) {
+        log.warn("TypeMismatchException Error: ", ex);
         return BaseErrorResponse.of(GlobalErrorCode.TYPE_MISMATCH);
     }
 
     // JSON 파싱 실패
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public BaseErrorResponse handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        log.warn("HttpMessageNotReadableException Error: ", ex);
         return BaseErrorResponse.of(GlobalErrorCode.INVALID_JSON);
     }
 }
