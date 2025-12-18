3ce59c02cc27240273a83bc262f81e9035ee0777
Chae-Yu <chaeyu2004@naver.com>
2025-12-18 16:04:40 +0900

    feat: Comment 엔티티 작성

---
+++ src/main/java/org/sopt/domain/comment/domain/Comment.java
@@ -0,0 +1,46 @@
+package org.sopt.domain.comment.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.domain.article.domain.Article;
+import org.sopt.domain.member.domain.Member;
+import org.sopt.global.entity.BaseTimeEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 300)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Member member;
+
+    @Builder
+    private Comment(String content, Article article, Member member) {
+        this.content = content;
+        this.article = article;
+        this.member = member;
+    }
+
+    public static Comment create(String content, Article article, Member member) {
+        return new Comment(content, article, member);
+    }
+
+    public void updateComment(String content){
+        this.content = content;
+    }
+}
