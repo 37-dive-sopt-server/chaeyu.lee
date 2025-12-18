861ba1ca5347f3f8f86729dbe2f86b911e6f1134
Chae-Yu <chaeyu2004@naver.com>
2025-12-18 16:41:13 +0900

    refactor: 댓글 작성 dto, 수정 dto 분리

--- src/main/java/org/sopt/domain/comment/service/CommentService.java
+++ src/main/java/org/sopt/domain/comment/service/CommentService.java
@@ -1,8 +1,10 @@
 package org.sopt.domain.comment.service;
 
 import org.sopt.domain.comment.dto.request.CommentCreateRequestDto;
+import org.sopt.domain.comment.dto.request.CommentUpdateRequestDto;
 import org.sopt.domain.comment.dto.response.CommentResponseDto;
 
 public interface CommentService {
     CommentResponseDto createComment(Long articleId, CommentCreateRequestDto commentCreateRequestDto);
+    CommentResponseDto updateComment(Long commentId, CommentUpdateRequestDto commentUpdateRequestDto);
 }
